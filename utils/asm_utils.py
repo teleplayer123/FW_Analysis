cbca18114cd85ec47c843d3b0197d16ce8bb4764
cole sashkin <csashceh1024@gmail.com>
2025-04-02 19:46:52 -0700

    update

---
+++ utils/asm_utils.py
@@ -0,0 +1,32 @@
+import ctypes
+import mmap
+
+
+def get_base_addr():
+    """Only works on linux"""
+    buf = mmap.mmap(-1, mmap.PAGESIZE, prot=mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
+
+    ftype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+    fpointer = ctypes.c_void_p.from_buffer(buf)
+
+    f = ftype(ctypes.addressof(fpointer))
+
+    buf.write( #need to get hex for opcodes
+        # mov eax, dword ptr fs:[30h]
+        # push eax
+        # mov eax, offset format
+        # push eax
+        # call printf
+        # pop ebx
+        # pop ebx
+        # ---- Example---
+        # b'\x8b\xc7'  # mov eax, edi
+        # b'\x83\xc0\x01'  # add eax, 1
+        # b'\xc3'  # ret
+    )
+
+    r = f(42)
+    print(r)
+
+    del fpointer
+    buf.close()
