d528246d3125d980212674cec97ca8c418d86e7d
Cole Sashkin <csashceh1024@gmail.com>
2025-04-14 13:14:07 -0700

    needs bounds checks

--- hex2bin.py
+++ hex2bin.py
@@ -1,11 +1,9 @@
-import re
-import os
 import sys
 
 
 def parse_hex_dump_long(filename):
-    lines = []
-    line_dict = {}
+    data_list = []
+    outfile = filename.replace(".log", ".bin")
     with open(filename, "r") as fh:
         for line in fh:
             if line == "":
@@ -13,7 +11,14 @@ def parse_hex_dump_long(filename):
             line = line.split("  ", maxsplit=1)[0]
             addr, data = line.split(" ", maxsplit=1)
             data = "".join(data.split(" "))
-            
+            line_list = []
+            for i in range(0, len(data), 2):
+                b = "".join([data[i], data[i+1]])
+                line_list.append(b)
+            data_list.append(bytes.fromhex("".join(reversed(line_list))))
+    with open(outfile, "w+b") as fh:
+        for data_bytes in data_list:
+            fh.write(data_bytes)
 
 if __name__ == "__main__":
     fname = sys.argv[1]
