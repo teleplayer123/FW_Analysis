0135eb753d0b98cdfd669e2eaa0e7a0ef825a382
cole sashkin <csashceh1024@gmail.com>
2025-04-14 19:25:57 -0700

    update

--- hex2bin.py
+++ hex2bin.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 
 def parse_hex_dump_long(filename):
@@ -20,6 +21,25 @@ def parse_hex_dump_long(filename):
         for data_bytes in data_list:
             fh.write(data_bytes)
 
-if __name__ == "__main__":
+def parse_uboot_dump(filename):
+    outfile = filename.replace(".log", ".bin")
+    new_fh = open(outfile, "w+b")
+    with open(filename, "r") as fh:
+        for line in fh.readlines():
+            line = line.split("  ", maxsplit=1)[0]
+            line = line.split(":", maxsplit=1)
+            if len(line) < 2:
+                continue
+            line = line[1].replace(" ", "")[:32]
+            print(line)
+            data = bytes.fromhex(line)
+            new_fh.write(data)
+    new_fh.close()
+
+
+def main():
     fname = sys.argv[1]
-    parse_hex_dump_long(fname)
+    parse_uboot_dump(fname)
+
+if __name__ == "__main__":
+    main()
