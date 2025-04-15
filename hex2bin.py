d8c8f7686be26f5f52dd5d0b07599c16f7f94b2b
cole sashkin <csashceh1024@gmail.com>
2025-04-14 22:49:34 -0700

    update

--- hex2bin.py
+++ hex2bin.py
@@ -2,6 +2,7 @@ import sys
 
 
 def parse_hex_dump_long(filename):
+    #TODO: this function is not reliable, need to fix byte order
     data_list = []
     outfile = filename.replace(".log", "_long.bin")
     with open(filename, "r") as fh:
