2cee3fdda557c75d4d8f7451d4894f9992ca6ba8
cole sashkin <csashceh1024@gmail.com>
2025-04-27 20:14:15 -0700

    update

--- file_formats/__init__.py
+++ file_formats/__init__.py
@@ -3,3 +3,12 @@ from hex_disasm.headers.intel_hex_file import IntelHexFile
 from elf_disasm.headers.elf32_header import ELF32
 from elf_disasm.headers.elf64_header import ELF64
 from uf2_disasm.headers.uf2_header import UF2
+
+
+__all__ = [
+    "SREC_File",
+    "IntelHexFile",
+    "ELF32",
+    "ELF64",
+    "UF2",
+]
