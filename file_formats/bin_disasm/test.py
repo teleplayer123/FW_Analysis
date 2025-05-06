0e9f15f88c3485946fac8812d27279fb24e8f408
cole sashkin <csashceh1024@gmail.com>
2025-05-05 22:14:14 -0700

    update

--- file_formats/bin_disasm/test.py
+++
@@ -1,5 +0,0 @@
-from headers.mpy_hdr import MPY
-
-fn = r"C:\binanalysis\disasm\adafruit_ducky.mpy"
-m = MPY(fn)
-print(m.mpy_hdr.magic)
--- file_formats/elf_disasm/test.py
+++
@@ -1,5 +0,0 @@
-from headers.elf32_header import ELF32
-
-fn = r"C:\pico_asm\build\pico_usb\pico_usb.elf"
-e = ELF32(fn)
-print(e.elf_hdr.e_ident)
