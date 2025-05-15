6756c701c59eea33541dee9ae5670f21cc80aa33
cole sashkin <csashceh1024@gmail.com>
2025-05-15 00:09:03 -0700

    update

--- file_formats/pdf_disasm/pdf_hdr.py
+++ file_formats/pdf_disasm/pdf_hdr.py
@@ -1,4 +1,5 @@
 import ctypes as ct
+import re
 import struct
 
 from file_sigs import MagicValues
@@ -19,5 +20,16 @@ class PDFHeader:
     MAGIC_VAL = MagicValues.PDF
     EOF_VAL = "%%EOF"
 
+    BOOL_REGX = re.compile(r"\b*(true|false)\b*")
+    STR_ASCII_REGX = re.compile(r"\([\w\d\s]+\)")
+    STR_HEX_REGX = re.compile(r"<[0-9A-Fa-f]+>")
+    NAME_REGX = re.compile(r"/[a-zA-Z0-9#]+")
+    ARRAY_REGX = re.compile(r"\[[^\]]+\]")
+    DICT_REGX = re.compile(r"<<[^\>]+>>")
+    STREAM_REGX = re.compile(r"stream")
+    ENDSTREAM_REGX = re.compile(r"endstream")
+    COMMENT_REGX = re.compile(r"%[^\n]*")
+
     def __init__(self, data):
         self.data = data
+
